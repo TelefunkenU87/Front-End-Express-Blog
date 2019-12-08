a84d43274d62ea314017160c1ac53b46058679c7
TelefunkenU87 <telewebb@gmail.com>
2019-12-08 16:26:17 -0500

    added module to save and get blog posts as a json file. Added permalink blog posts pages. Added a form page to add posts.

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
 var workRouter = require('./routes/work');
+var postRouter = require('./routes/post');
+var newPostRouter = require('./routes/newPost');
 
 var app = express();
 
@@ -25,6 +27,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 app.use('/work', workRouter);
+app.use('/post', postRouter);
+app.use('/newpost', newPostRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+let repo = require('../models/postRepository');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'tele-funk home' });
+  res.render('index', { 
+    title: 'tele-funk home',
+  posts: repo.getPosts()
+ });
 });
 
 module.exports = router;
